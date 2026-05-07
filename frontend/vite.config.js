f08e77426cb0555d459f8414440e6703fa2c29a7
Shane <shane.lin@langlive.com>
2026-05-07 19:09:04 +0800

    refactor: update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    conditions: ["import", "module", "browser", "default"],
+  },
   server: {
-    port: 5173
-  }
+    port: 5173,
+  },
 });
